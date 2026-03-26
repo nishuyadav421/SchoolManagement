884851323fe3b0d0253289c930de08e801201a27
Nishu Kumar Yadav <nishuy421@gmail.com>
2026-03-27 04:44:24 +0530

    Final DB update for Railway

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -2,12 +2,21 @@ package util;
 import java.sql.*;
 
 public class DBConnection {
-    public static Connection getConnection() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/school_db",
-            "root",
-            "nishu@12345"
-        );
-    }
+	public static Connection getConnection() throws Exception {
+	    Class.forName("com.mysql.cj.jdbc.Driver");
+	    
+	   
+	    String url = System.getenv("MYSQL_URL"); 
+	    String user = System.getenv("MYSQLUSER");
+	    String pass = System.getenv("MYSQLPASSWORD");
+	    
+	   
+	    if (url == null) {
+	        url = "jdbc:mysql://localhost:3306/school_db"; 
+	        user = "root";
+	        pass = "nishu@12345";
+	    }
+
+	    return DriverManager.getConnection(url, user, pass);
+	}
 }
