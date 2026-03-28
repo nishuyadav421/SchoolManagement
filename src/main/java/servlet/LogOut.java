45d66164970252a3355f22e8803fe9d0123cad08
Nishu Kumar Yadav <nishuy421@gmail.com>
2026-03-28 07:32:43 +0530

    Update: Added new features/fixes in School Management

---
+++ src/main/java/servlet/LogOut.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LogOut extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+		
+		
+		
+	}
+
+}
